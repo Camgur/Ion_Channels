15fc8068ca5d47f3d55d004a69778f903103a060
Cameron <156104897+Camgur@users.noreply.github.com>
2024-06-13 18:14:26 -0400

    Change PATH

--- Code/opt_all.py
+++ Code/opt_all.py
@@ -31,7 +31,7 @@ for filename in os.listdir(folder):
         atoms = read(file)
 
         # Setting the MACE-MP-0 Calculator
-        calculator = MACECalculator(model_paths='~/projects/rrg-ravh011/cgurwell/Ion_Channels/2024-01-07-mace-128-L2_epoch-199.model',
+        calculator = MACECalculator(model_paths='/home/cgurwell/projects/rrg-ravh011/cgurwell/Ion_Channels/2024-01-07-mace-128-L2_epoch-199.model',
                                     dispersion=False, device='cuda', default_dtype='float64')
         atoms.calc = calculator
 
